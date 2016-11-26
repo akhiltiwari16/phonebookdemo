fe33197661d2ffae1812b06f16e4c39153366ed7
akhiltiwari16 <akhiltiwari16@yahoo.com>
2016-11-26 22:04:05 +0530

    Modified configuration

--- config/config.js
+++ config/config.js
@@ -6,6 +6,6 @@ config.mongo = {
   port: 27017
 }
 
-config.mongo.url = 'mongodb://' + config.mongo.hostName + ':' + config.mongo.port + '/' + config.mongo.database;
-
+//config.mongo.url = 'mongodb://' + config.mongo.hostName + ':' + config.mongo.port + '/' + config.mongo.database;
+config.mongo.url = 'mongodb://phonebookusername:password@ds159767.mlab.com:59767/phonebookdemodb';
 module.exports = config;
